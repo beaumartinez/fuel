ddaf6402d40b63e233308ebbb8b16ba85a59fc54
Beau <beau@beaumartinez.com>
2012-12-26 15:00:26 +0000

    Initial dump

---
+++ fuel.js
@@ -0,0 +1,135 @@
+#! /usr/bin/env casperjs
+
+var casper = require('casper').create({
+    logLevel: 'debug',
+    verbose: false,
+    viewportSize: {
+        width: 1280,
+        height: 800,
+    },
+    pageSettings: {
+        loadImages: false,
+    },
+    waitTimeout: 10000,
+});
+var fs = require('fs');
+
+var LOGIN_URL = 'http://nikeplus.nike.com/plus/login/';
+
+(function() {
+    var currentUrl;
+    var email;
+    var frameName;
+    var fuel = new Object();
+    var password;
+
+    function waitForUrlChange() {
+        casper.waitFor(function() {
+            return casper.getCurrentUrl() !== currentUrl;
+        }, function() {
+            currentUrl = casper.getCurrentUrl();
+        });
+    }
+
+    function getData() {
+        casper.then(function doShitWithData() {
+            var date = casper.evaluate(function() {
+                return window.np.baked_data.activity.localStart.utcTime;
+            });
+
+            var data = casper.evaluate(function() {
+                return window.np.baked_data.dataPoints;
+            });
+            data = data.map(function(x) {
+                return x.fuel;
+            });
+
+            fuel[date] = data;
+
+            var serializedFuel = JSON.stringify(fuel, null, 4);
+            fs.write('fuel.json', serializedFuel, 'w');
+
+            var serializedDate = new Date(date);
+            console.log('Wrote', serializedDate);
+        });
+
+        casper.then(function clickPreviousLink() {
+            casper.click('a.previous');
+            casper.then(waitForUrlChange);
+        });
+
+        casper.then(getData);
+    }
+
+    (function() {
+        casper.start(LOGIN_URL, function waitTillLoginFrameVisible() {
+            casper.waitUntilVisible('#checkers', function() {
+                frameName = casper.evaluate(function() {
+                    var frame = document.getElementById('checkers');
+
+                    return frame.name;
+                });
+            });
+
+            currentUrl = casper.getCurrentUrl();
+        });
+
+        casper.then(function submitLoginForm() {
+            casper.withFrame(frameName, function() {
+                casper.evaluate(function(email, password) {
+                    var form = document.querySelector('#loginform form');
+
+                    var emailNode = form.querySelector('input[type=email]');
+                    emailNode.value = email;
+
+                    var passwordNode = form.querySelector('input[type=password]');
+                    passwordNode.value = password;
+
+                    // Submitting like this because form.submit() doesn't work
+                    var submit = form.querySelector('input[type=submit]');
+                    submit.click();
+                }, {
+                    email: email,
+                    password: password,    
+                });
+            });
+
+            casper.then(waitForUrlChange);
+        });
+
+        casper.then(function clickActivityLink() {
+            casper.click('a[title=Activity]');
+
+            casper.then(waitForUrlChange);
+        });
+
+        casper.then(function clickMonthLink() {
+            casper.click('a.month');
+
+            casper.then(waitForUrlChange);
+        });
+
+        casper.then(function clickLastSyncBar() {
+            casper.evaluate(function() {
+                var links = document.querySelectorAll('a.bar_shim');
+                var lastLink = links[links.length - 1];
+
+                window.location = lastLink.href;
+            });
+
+            casper.then(waitForUrlChange);
+        });
+
+        casper.then(getData);
+
+        if (casper.cli.args.length < 2) {
+            console.log('Usage: ./fuel.js <email> <password>');
+            casper.exit();
+        }
+
+        email = casper.cli.args[0];
+        password = casper.cli.args[1];
+
+        casper.run();
+    })();
+})();
---
+++ readme.md
@@ -0,0 +1,146 @@
+# Gimme my NikeFuel
+
+If you're like me, you feel real smug wearing a [FuelBand][fuelband] and
+wouldn't mind access to its data. Unfortunately, there's no easy way to do
+that. [Nike won't publish an API][api], so they've given me No Choice™ and I've
+built a scraper.
+
+Now [their web app][web app] is no GeoCities website—it's a JavaScript-heavy
+"modern" web app, with iframes and all types of scary magic. Its graphs use
+canvas, so DOM scraping is out of the question. However we *can* get the graph
+data from a global JavaScript variable.
+
+(If it wasn't in a global variable, I sure hope you could programmatically
+debug PhantomJS's WebKit browser's JavaScript engine, otherwise you'd be pretty
+fucked. We'd have to parse the JavaScript code responsbile and extract the
+value somehow.)
+
+This script extracts all your FuelBand data to a JSON file. The JSON file is a
+map of days (as JavaScript timestamps) to an array of Fuel. The array contains
+16 elements, one for each 15 minutes of the day. (Hey, that's even more
+granular than the graphs on the app!)
+
+This is what it looks like:
+
+    {
+        "1356393600000": [
+            13,
+            57,
+            0,
+            0,
+            2,
+            1,
+            0,
+            0,
+            0,
+            0,
+            3,
+            2,
+            22,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            3,
+            0,
+            1,
+            0,
+            1,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            2,
+            0,
+            8,
+            15,
+            33,
+            2,
+            105,
+            99,
+            31,
+            32,
+            23,
+            5,
+            35,
+            76,
+            76,
+            21,
+            51,
+            34,
+            30,
+            37,
+            44,
+            42,
+            50,
+            7,
+            6,
+            5,
+            18,
+            62,
+            4,
+            2,
+            0,
+            2,
+            1,
+            0,
+            3,
+            0,
+            53,
+            51,
+            7,
+            2,
+            8,
+            49,
+            18,
+            7,
+            3,
+            59,
+            7,
+            20,
+            16,
+            28,
+            30,
+            62,
+            41,
+            32,
+            17,
+            24,
+            32,
+            9,
+            8,
+            14,
+            4,
+            17,
+            28
+        ],
+    }
+
+Use with care.
+
+## Requirements
+
+- [CasperJS](http://casperjs.org/)
+    - [PhantomJS](http://phantomjs.org/)
+
+And a FuelBand and Nike+ account, of course.
+
+## Usage
+
+    fuel.js <email> <password>
+
+## Notes
+
+The web app is a bit buggy in places—I've seen it skip days (yes, the web
+app—not the scraper) when you click "previous".
+
+[api]: https://twitter.com/NikeFuel/status/205424488836370433
+[fuelband]: http://www.nike.com/us/en_us/lp/nikeplus-fuelband
+[web app]: http://nikeplus.nike.com/plus/
